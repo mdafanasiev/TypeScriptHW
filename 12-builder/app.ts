7ced9629cd10a12f8a2e66994d44cc09876bf6d6
max <maxaf278@gmail.com>
2025-09-20 14:13:40 +0300

    Дз 12 модуля

---
+++ 12-builder/app.ts
@@ -0,0 +1,48 @@
+import axios, { Axios, AxiosHeaders } from 'axios';
+
+enum QueryTypes {
+  GET = "GET",
+  POST = "POST",
+  DELETE = "DELETE",
+}
+
+class QueryGenerator {
+	private url: string;
+	private method: QueryTypes;
+	private headers: AxiosHeaders;
+	private body: string; 
+	
+
+	setMethod(qType: QueryTypes) {
+		this.method = qType;
+		return this;
+	}
+
+	setURL(url: string) {
+		this.url = url;
+		return this
+	}
+
+	setHeaders(headers: AxiosHeaders) {
+		this.headers = headers;
+		return this;
+	}
+	
+	setBody(body: string) {
+		this.body = body;
+		return this;
+	}
+
+	async exec() {
+		const { data: res } = await axios.request({
+		url: this.url,
+		method: this.method,
+		headers: this.headers,
+		data: this.body,
+		});
+		return res;
+	}
+}
+
+const qGenerator = new QueryGenerator();
+const res = qGenerator.setURL("https://api.escuelajs.co/api/v1/products").setMethod(QueryTypes.GET).exec();
